8c2e850ae48d9d3b4250916af49d3c3e112b8b40
ljsinke <l.j.sinke@lumc.nl>
2021-01-28 16:18:26 +0100

    Added option to change font sizes and updated vignette

--- R/omixerSheet.R
+++ R/omixerSheet.R
@@ -6,6 +6,8 @@
 #'
 #' @param omixerLayout Randomized sample list
 #' @param group Colour-coding indicator
+#' @param group.text.size Change size of group text (default: 3.5)
+#' @param sample.text.size Change size of sample text (default: 4)
 #'
 #' @return PDF of sample layout in working directory
 #'
@@ -39,7 +41,8 @@
 #' 
 #' omixerSheet(omixerLayout)
 
-omixerSheet <- function(omixerLayout=omixerLayout, group) {
+omixerSheet <- function(omixerLayout=omixerLayout, group, 
+    group.text.size = 3.5, sample.text.size = 4) {
 
     ## Set labels
     sampleId <- NULL
@@ -66,9 +69,10 @@ omixerSheet <- function(omixerLayout=omixerLayout, group) {
         geom_tile(aes(x=column, y=row, fill=factor(bottom)), colour="grey20", size=1.5,
             show.legend=FALSE) + coord_equal() +
         geom_text(aes(label=ifelse(is.na(bottom), "", as.character(bottom))),
-            colour="grey30", size=3.5, nudge_y=0.2) +
+            colour="grey30", size=group.text.size, nudge_y=0.2) +
         geom_text(aes(label=ifelse(is.na(top), "", as.character(top))),
-            colour="grey30", fontface="bold", size=4, nudge_y=-0.1) +
+            colour="grey30", fontface="bold", size=sample.text.size, 
+            nudge_y=-0.1) +
         scale_fill_manual(values=cbPalette, drop=F) +
         scale_x_discrete(name="",
             limits=factor(c(min(as.numeric(omixerLayout$column)):
